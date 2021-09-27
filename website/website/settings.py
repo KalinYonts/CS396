eb4c02842ff6b1c6a6bd12ae008980da492ce011
Kalin Yonts <kalinyonts@yahoo.com>
2021-09-26 20:19:23 -0500

    Updated STATIC_ROOT

--- website/website/settings.py
+++ website/website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
